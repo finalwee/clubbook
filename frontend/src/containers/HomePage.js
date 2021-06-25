02cc5bf59bf29accd55ffa9b7fedfac7c19e082e
shihchunjen <shihchunjen@gmail.com>
2021-06-25 13:12:34 +0800

    HomePage 3 edition

--- frontend/src/containers/HomePage.js
+++ frontend/src/containers/HomePage.js
@@ -1,17 +1,27 @@
 import { useState } from 'react';
 import {Typography,CssBaseline,} from '@material-ui/core';
 import SideBar from "../components/SideBar";
-// import { SearchBar } from 'react-native-elements';
+import SearchBar from "material-ui-search-bar";
+
 
 function HomePage() {
     
+    const [clubsearch, setClubSearch] = useState('');
+
     return(
-        <div>
+        <div className="homepage">
             <SideBar/>
-            <div style={{ padding: 16, margin: 'auto', maxWidth: 600 }}>
+            <div className="clubsearch">
+                <SearchBar
+                    value={clubsearch}
+                    onChange={(newValue) => setClubSearch(() => {return newValue;})}
+                    onRequestSearch={() => {}}
+                />
+            </div>
+            <div className="main" style={{ padding: 16, margin: 'auto', maxWidth: 600 }}>
                 <CssBaseline />
                 <Typography variant="h4" align="center" component="h1" gutterBottom>
-                &#x2663; Club Book &#x1F4DA;
+                    &#x2663; Club Book &#x1F4DA;
                 </Typography>
             </div>
         </div>
